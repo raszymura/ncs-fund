d7af6f013a9cba0479d674b10b326040bb68a8cd
Rafał Szymura <raszymura@gmail.com>
2024-05-15 00:37:47 +0200

    fund_less3_exer1 custom files

--- v2.x.x/lesson3/fund_less3_exer1/src/main.c
+++ v2.x.x/lesson3/fund_less3_exer1/src/main.c
@@ -1,10 +1,12 @@
 #include <zephyr/kernel.h>
 #include <zephyr/sys/printk.h>
+#include "myfunction.h"
 
 int main(void)
 {
-	while(1) {
-		printk("Hello World!\n\r");
+	int a = 3, b = 4;
+	while(1){
+		printk("The sum of %d and %d is %d\n\r", a, b, sum(a,b));
 		k_msleep(1000);
 	}
 }
