467b5c488f65f1af398727aa18fd4a99b333e02b
Rafał Szymura <raszymura@gmail.com>
2024-05-15 00:51:20 +0200

    fund_less3_exer1 Custom configurations

--- v2.x.x/lesson3/fund_less3_exer1/src/main.c
+++ v2.x.x/lesson3/fund_less3_exer1/src/main.c
@@ -1,12 +1,21 @@
 #include <zephyr/kernel.h>
 #include <zephyr/sys/printk.h>
+#ifdef CONFIG_MYFUNCTION
 #include "myfunction.h"
+#endif
 
-int main(void)
+
+void main(void)
 {
-	int a = 3, b = 4;
+	
 	while(1){
-		printk("The sum of %d and %d is %d\n\r", a, b, sum(a,b));
+		#ifdef CONFIG_MYFUNCTION
+		int a = 3, b = 4;
+			printk("The sum of %d and %d is %d\n\r", a, b, sum(a,b));
+		#else
+			printk("MYFUNCTION not enabled\r\n");
+			return;
+		#endif
 		k_msleep(1000);
 	}
 }
